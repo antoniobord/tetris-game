2422c64cf4621c22a2d7e70743960973aa646e1a
Antonio Bordamonte <antonioaborda@gmail.com>
2024-06-21 23:48:06 -0400

    implemented score system

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ score_surface = title_font.render("Score", True, Colors.white)
 next_surface = title_font.render("Next", True, Colors.white)
 game_over_surface = title_font.render("GAME OVER", True, Colors.white)
 
+
 score_rect = pygame.Rect(320, 55, 170, 60)
 next_rect = pygame.Rect(320, 215, 170, 180)
 
@@ -37,6 +38,7 @@ while True:
                 game.move_right()
             if event.key == pygame.K_DOWN and game.game_over == False:
                 game.move_down()
+                game.update_score(0, 1)
             if event.key == pygame.K_UP and game.game_over == False:
                 game.rotate()
         if event.type == GAME_UPDATE and game.game_over == False:
@@ -45,6 +47,8 @@ while True:
 
     
     # drawing
+    score_value_surface = title_font.render(str(game.score), True, Colors.white)
+
     screen.fill(Colors.dark_blue)
     screen.blit(score_surface, (365, 20, 50, 50)) # tuple = (x cord, y cord, width, height)
     screen.blit(next_surface, (375, 180, 50, 50)) # blit = block image transfer
@@ -53,6 +57,8 @@ while True:
         screen.blit(game_over_surface, (320, 450, 50, 50))
 
     pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+    screen.blit(score_value_surface, score_value_surface.get_rect(centerx = score_rect.centerx, 
+        centery = score_rect.centery))
     pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
     game.draw(screen)
 
